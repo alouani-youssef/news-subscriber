bc34bc56a4617ad640d2520de850a0220dd260ca
alouani-youssef <alouaniyoussef.dev@gmail.com>
2024-10-21 16:54:46 +0100

    chore: setup final project structure

---
+++ src/common/validation/environment.validation.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+import { EnvironmentType } from "../types";
+
+const Config = z.object({
+    NODE_ENV: z.nativeEnum(EnvironmentType).default(EnvironmentType.development),
+    PORT: z.preprocess((port) => Number(port), z.number()),
+
+    POSTGRES_HOST: z.string(),
+    POSTGRES_USERNAME: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_PORT: z.preprocess((port) => Number(port), z.number()),
+    POSTGRES_DATABASE: z.string(),
+
+    REDIS_HOST: z.string(),
+    REDIS_USERNAME: z.string(),
+    REDIS_PASSWORD: z.string(),
+    REDIS_PORT: z.preprocess((port) => Number(port), z.number()),
+    REDIS_DATABASE: z.preprocess((port) => Number(port), z.number()),
+
+    SENTRY_URL: z.string().url()
+});
+
+export function EnvironmentVariablesValidation(
+    config: Record<string, unknown>
+) {
+    return Config.parse(config);
+}
+
+export type EnvironmentVariables = z.infer<typeof Config>;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,85 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
+import {
+  NestFactory,
+  BaseExceptionFilter,
+  HttpAdapterHost,
+} from '@nestjs/core';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as Sentry from '@sentry/node';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { hostname } from "os";
+import "reflect-metadata";
+
+import { EnvironmentVariables } from "src/common/validation";
+import { AppModule } from "./modules/app/app.module";
+
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('NEWS SUBSCRIBER API Documentation')
+    .setDescription('PROVIDED APIS FOR NEWS SUBSCRIBER APPLICATION')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
+
+
+function setupValidationPipes(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+};
+
+async function createApp() {
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    forceCloseConnections: true,
+    bufferLogs: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    })
+  );
+  app.setGlobalPrefix("api", {
+    exclude: ["api", "health"],
+  });
+  return app;
+}
+
+function setupSentry(app, configService: ConfigService<EnvironmentVariables>) {
+  const SENTRY_DSN = configService.get('SENTRY_URL' as const);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  Sentry.init({ dsn: SENTRY_DSN });
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await createApp();
+
+  const configService =
+    app.get<ConfigService<EnvironmentVariables>>(ConfigService);
+
+  setupValidationPipes(app);
+  setupSwagger(app);
+  setupSentry(app, configService);
+
+  const PORT = configService.get("PORT");
+  const NODE_ENV = configService.get("NODE_ENV");
+
+  await app.listen(PORT);
+  console.info(
+    `NEWS SUBSCRIBER HAS STARTED AND IT IS LISTENING ON PORT : ${PORT} IN SERVER : ${hostname()} IN THE MODE OF : ${NODE_ENV}`
+  );
 }
+
 bootstrap();
