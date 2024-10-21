ee90b846a880765498d14bebfd58ad886f78436e
alouani-youssef <alouaniyoussef.dev@gmail.com>
2024-10-21 17:01:09 +0100

    chore: configuration module injection

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvironmentVariablesValidation } from 'src/common/validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      validate: EnvironmentVariablesValidation,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
